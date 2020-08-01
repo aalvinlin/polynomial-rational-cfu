23b97ca7070803e2d8e798b2864e24ca9ae01f9d
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-07-31 21:07:04 -0700

    add test examples and swap left/right points if needed

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -17,11 +17,27 @@ const Grid = () => {
     }
 
     const LineWithArrows = ({xStart, yStart, xEnd, yEnd, color}) => {
+
+        const slope = (yEnd - yStart) / (xEnd - xStart);
+
+        // swap left and right points if needed so that (xEnd, yEnd) is on the right
+        if (xEnd < xStart)
+            {
+                let xTemp = xStart;
+                let yTemp = yStart;
+
+                xStart = xEnd;
+                yStart = yEnd;
+
+                xEnd = xTemp;
+                yEnd = yTemp;
+            }
+
         return (
             <>
-                <polygon points={`${xStart},${yStart} ${xStart + 10},${yStart + 10} ${xStart + 10},${yStart - 10}`} fill="#99CCDD" />
+                <polygon points={`${xStart},${yStart} ${xStart + 10},${yStart + 10} ${xStart + 10},${yStart - 10}`} fill={color} />
                 <Line xStart={xStart} yStart={yStart} xEnd={xEnd} yEnd={yEnd} color={color} />;
-                <polygon points={`${xEnd},${yEnd} ${xEnd - 10},${yEnd + 10} ${xEnd - 10},${yEnd - 10}`} fill="#99CCDD" />
+                <polygon points={`${xEnd},${yEnd} ${xEnd - 10},${yEnd + 10} ${xEnd - 10},${yEnd - 10}`} fill={color} />
             </>
         );
     }
@@ -34,12 +50,19 @@ const Grid = () => {
         return <LineWithArrows xStart={x} yStart="0" xEnd={x} yEnd="800" color="#99CCDD" />;
     }
 
+    // convert from grid coordinates to actual SVG coordinates!
+
     return (
         <svg viewBox="0 0 800 800">
             {[...Array(20)].map((item, i) => horizontalGridLine(10 + 50 * i))}
             {[...Array(20)].map((item, i) => verticalGridLine(10 + 50 * i))}
             {xAxis(410)}
             {yAxis(410)}
+
+            <LineWithArrows xStart={50} yStart={20} xEnd={300} yEnd={350} color="#CC6699" />;
+            <LineWithArrows xStart={650} yStart={200} xEnd={40} yEnd={100} color="#CC6699" />;
+            <LineWithArrows xStart={700} yStart={400} xEnd={700} yEnd={60} color="#CC6699" />;
+            <LineWithArrows xStart={500} yStart={150} xEnd={40} yEnd={150} color="#CC6699" />;
         </svg>
     )
 }
