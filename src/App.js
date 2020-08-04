1341d7033e83b636a4f67f81691261c7851434ad
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-08-03 20:33:54 -0700

    add asymptotes and function parts as arguments to Grid component

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import React from "react";
 import Grid from "./components/Grid";
 
 const App = () => {
+
+  let verticalAsymptotes = [];
+  let horizontalAsymptotes = [];
+  let obliqueAsymptotes = [];
+  
+  let curvedFunctionParts = [];
+
   return (
     <>
-      <Grid xMax={800} yMax={800} horizontalSpacing={40} verticalSpacing={40} horizontalPadding={25} verticalPadding={25} />
+      <Grid xMax={800} yMax={800} horizontalSpacing={40} verticalSpacing={40} horizontalPadding={25} verticalPadding={25} verticalAsymptotes={verticalAsymptotes} horizontalAsymptotes={horizontalAsymptotes} obliqueAsymptotes={obliqueAsymptotes} curvedFunctionParts={curvedFunctionParts} />
     </>
   );
 }
