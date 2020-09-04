78585096057ef1234b484d78a7d00acd51a4acc9
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-09-03 23:52:07 -0700

    begin problem picker component

---
+++ src/components/ProblemSelector.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ProblemSelector = () => {
+
+  return (
+    <>
+      <p>Choose an equation type to practice:</p>
+
+      <p>Polynomial</p>
+
+      <p>Rational Expression</p>
+
+      <p>Asymptote Type:</p>
+
+      <ul>
+          <li>none</li>
+          <li>Horizontal</li>
+          <li>Vertical</li>
+          <li>Oblique</li>
+      </ul>
+
+      <p>Holes</p>
+
+    </>
+  );
+}
+
+export default ProblemSelector;
