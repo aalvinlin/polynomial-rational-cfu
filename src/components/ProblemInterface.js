1d23e40408687cd5ac5f5c5cf085e99cd9fc1b90
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-10-07 20:16:59 -0700

    create divs to hold parts to ProblemInterface component

--- src/components/ProblemInterface.js
+++ src/components/ProblemInterface.js
@@ -1,10 +1,19 @@
 import React from "react";
+import Equation from "./Equation";
+import Grid from "./Grid";
 
 const ProblemInterface = () => {
 
   return (
     <>
-      
+      <Equation />
+
+      <div className="gridAndInputs">
+          <Grid />
+          <div className="inputs">
+              
+          </div>
+      </div>
     </>
   );
 }
