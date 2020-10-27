37de9c2e2f1ecaded748a2e231ffcef69d35f7e8
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-10-26 22:17:37 -0700

    continue implementing formatEquation

--- src/components/Equation.js
+++ src/components/Equation.js
@@ -3,12 +3,19 @@ import React from "react";
 const Equation = ({equationData}) => {
 
   function formatEquation(equation) {
-    
+
+    return equation.map((coefficient, id) => {
+
+      let power = 0;
+
+      return (coefficient + "x")
+
+    });
   }
 
   return (
     <div className="equation">
-      <h1>{equationData.equation}</h1>
+      <h1>{formatEquation(equationData.equation)}</h1>
     </div>
   );
 }
